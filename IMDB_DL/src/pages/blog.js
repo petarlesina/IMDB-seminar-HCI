6876b96e72714047867033f650fc4e8834fc1908
petarlesina <56306108+petarlesina@users.noreply.github.com>
2020-01-08 20:04:57 +0100

    small changes

--- IMDB_DL/src/pages/blog.js
+++ IMDB_DL/src/pages/blog.js
@@ -1,16 +1,43 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import { Link } from "gatsby"
 
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    
+
     return (
         <div>
             <Layout>
             <h1>blog</h1>
-            <p>
-                Tekst TekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekstTekst
-            </p>
+            <ol>
+
+                {data.allMarkdownRemark.edges.map((edge) =>{
+                    return (
+                        <li>
+                            <h3>{edge.node.frontmatter.title}</h3>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
+                    )
+                })}
+
+            </ol>
             </Layout>
         </div>
     )
