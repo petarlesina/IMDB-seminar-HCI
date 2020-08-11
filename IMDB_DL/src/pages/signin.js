aaed2098f9fa95a8d48f058dfc306c1dffa6187c
petarlesina <56306108+petarlesina@users.noreply.github.com>
2020-08-11 11:57:17 +0200

    login page

--- IMDB_DL/src/pages/signin.js
+++ IMDB_DL/src/pages/signin.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Link } from "gatsby"
 
 import Layout from '../components/layout'
-
+import style from './signin.css'
 
 const SignInPage = () => {
     return (
         <div>
             <Layout>
-            <h1>prijava</h1>
+            {/* <h1>prijava</h1> */}
+            <form class="box" action="" method="post">
+                <h1>Login</h1>
+                <input type="text" name="" placeholder="Username"></input>
+                <input type="password" name="" placeholder="Password"></input>
+                <input type="submit" name="" value="Log in"></input>
+
+            </form>
             </Layout>
         </div>
 
