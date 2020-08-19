2ab85394e3ede163f8eb360a6b872a5412c5ba5d
petarlesina <56306108+petarlesina@users.noreply.github.com>
2020-08-19 16:21:44 +0200

    albums page

--- IMDB_DL/src/pages/albums.js
+++ IMDB_DL/src/pages/albums.js
@@ -14,90 +14,112 @@ const AlbumsPage = () => {
         {
           path: "",
           title: "album1",
-          text: "Here you can discover new albums, who knows what you can find!",
+          category: "Rock",
           photo: logo,
+          year: "1988",
+          madeby: "artist1"
         },
         {
           path: "",
           title: "album2",
-          text: "Discover new or rediscover old groups",
+          category: "Jazz",
           photo: photo,
+          year: "1981",
+          madeby: "artist4"
         },
         {
           path: "",
           title: "album3",
-          text: "Read the comments made by other people",
+          category: "Techno",
           photo: photo,
+          year: "1948",
+          madeby: "artist3"
         },
         {
             path: "",
             title: "album4",
-            text: "Here you can discover new albums, who knows what you can find!",
+            category: "Country",
             photo: photo,
+            year: "1969",
+            madeby: "artist1"
           },
           {
             path: "",
             title: "album5",
-            text: "Discover new or rediscover old groups",
+            category: "Rock",
             photo: photo,
+            year: "1985",
+            madeby: "artist1"
           },
           {
             path: "",
             title: "album6",
-            text: "Read the comments made by other people",
+            category: "Classical",
             photo: photo,
+            year: "1928",
+            madeby: "artist4"
           },
           {
             path: "",
             title: "album7",
-            text: "Read the comments made by other people",
+            category: "Rock",
             photo: photo,
+            year: "2000",
+            madeby: "artist3"
           },
           {
             path: "",
             title: "album8",
-            text: "Read the comments made by other people",
+            category: "Metal",
             photo: photo,
+            year: "1999",
+            madeby: "artist3"
           },
-          {
-            path: "",
-            title: "album9",
-            text: "Read the comments made by other people",
-            photo: photo,
-          },
+          // {
+          //   path: "",
+          //   title: "album9",
+          //   category: "Rock",
+          //   photo: photo,
+          //   year: "2020",
+          //   madeby: "artist4"
+          // },
       ]
 
     return (
         <div>
             <Layout>
             <SEO title="Albums" keywords="Albums" /> 
-            <div class="card-container2">
+            <div class="card-container3">
               
-              <div class="heading2">
+              <div class="heading3">
                 <h1>Albums</h1>
               </div>
 
-              <div class="searchWrapper">
+              <div class="searchWrapper3">
                 <input
                     type="text"
                     name="searchBar"
-                    class="searchBar"
+                    class="searchBar3"
                     placeholder="Search albums"
                 /> 
                 
             </div>
 
-              <div class="card-row2">
+              <div class="card-row3">
               {cardArray.map(card => (
-            <Link to="/" className="card2">
-            <div class="card-header2">
+            <Link to="/" className="card3">
+            <div class="card-header3">
                   <h1>{card.title}</h1>
             </div>
 
-            <div class="card-body2">
-              <img class="picture" src={card.photo} alt="Italian Trulli"></img>
-                <p class="tekst-kartice">{card.text}</p>
-                {/* <Link to={card.path} className="btn2" key={card.path}> Read more </Link> */}
+            <div class="card-body3">
+              <img class="picture3" src={card.photo} alt="Italian Trulli"></img>
+                <p class="kategorija-kartice">Category: {card.category}</p>
+                <p class="godina-kartice">Year: {card.year}</p>
+                <p class="madeby-kartice">
+                  <Link to={card.path} className="link-artist" key={card.path}> {card.madeby} </Link>
+                  </p>
+                
                 </div>
                 </Link>
             ))}
