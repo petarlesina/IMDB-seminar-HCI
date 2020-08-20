06c0fb8f3ee0242edfaa8bb8eaf41511c96b584f
petarlesina <56306108+petarlesina@users.noreply.github.com>
2020-08-20 17:11:52 +0200

    clickalble homepage fix

---
+++ IMDB_DL/src/pages/temp/album-temp.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from "gatsby"
+import SEO from "../../components/seo"
+import Layout from "../../components/layout"
+
+
+const AlbumsPage = () => {
+
+    
+
+    return (
+            <div>
+            <Layout>
+            <SEO title="Album" keywords="Album" /> 
+            
+
+
+
+
+            </Layout>
+            </div>
+        
+
+    )
+
+}
+
+export default AlbumsPage
